6c3ed3908d61465101f7157fc7d33f42ee994967
xushuyang <845158228@qq.com>
2021-07-22 20:16:12 +0800

    dev

--- kafka-desktop/src/main/java/com/tuna/desktop/kafka/controller/AppController.java
+++ kafka-desktop/src/main/java/com/tuna/desktop/kafka/controller/AppController.java
@@ -1,56 +1,65 @@
 package com.tuna.desktop.kafka.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
 
-    @FXML
-    private TreeView treeView;
     @FXML
     private RadioMenuItem kafka;
     @FXML
     private RadioMenuItem zookeeper;
     @FXML
     private RadioMenuItem redis;
-
-    public void onTreeViewClick() {
-        System.out.println("onTreeViewClick");
-    }
+    @FXML
+    private VBox contentBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ToggleGroup group = new ToggleGroup();
         kafka.setToggleGroup(group);
-        kafka.setSelected(true);
         zookeeper.setToggleGroup(group);
         redis.setToggleGroup(group);
 
-        kafka.setOnAction(event -> {
+        kafka.setSelected(true);
+        changePanel("kafka.fxml", resources);
 
+        kafka.setOnAction(event -> {
+            changePanel("kafka.fxml", resources);
         });
 
         zookeeper.setOnAction(event -> {
-
+            changePanel("zookeeper.fxml", resources);
         });
 
         redis.setOnAction(event -> {
-
+            changePanel("redis.fxml", resources);
         });
 
-        TreeItem<String> root = new TreeItem<String>(resources.getString("cluster"));
-        root.setExpanded(true);
-        root.getChildren().addAll(
-            new TreeItem<String>("Item 1"),
-            new TreeItem<String>("Item 2"),
-            new TreeItem<String>("Item 3")
-        );
-        treeView.setRoot(root);
     }
+
+    public void changePanel(String fxml, ResourceBundle bundle) {
+        try {
+            if (contentBox.getChildren().size() > 1) {
+                contentBox.getChildren().remove(1);
+            }
+            Node rootLayout = FXMLLoader.load(getClass().getResource(fxml), bundle);
+            VBox.setVgrow(rootLayout, Priority.ALWAYS);
+            contentBox.getChildren().add(rootLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
