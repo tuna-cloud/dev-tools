0205466f0b52f07314eb86e595225b37e778cf7e
xushy <29203307+tuna-cloud@users.noreply.github.com>
2022-03-04 17:34:52 +0800

    java基本工具 - Json格式化工具集成 - 时间戳转换工具集成

--- tools-fiddler/src/main/java/com/tuna/tools/fiddler/Client.java
+++ tools-fiddler/src/main/java/com/tuna/tools/fiddler/Client.java
@@ -1,50 +1,30 @@
 package com.tuna.tools.fiddler;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.*;
 import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetClientOptions;
 import io.vertx.core.net.NetSocket;
 
 public class Client {
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        NetClientOptions options = new NetClientOptions();
+        HttpClientOptions options = new HttpClientOptions();
         options.setTrustAll(true);
-        NetClient client = vertx.createNetClient(options);
-        client.connect(8090, "127.0.0.1", result -> {
-            if (result.succeeded()) {
-                System.out.println("connect success");
-                NetSocket socket = result.result();
-                socket.handler(buf -> {
-                    System.out.println(buf.toString());
-                });
-                socket.upgradeToSsl(rt -> {
-                    if (rt.succeeded()) {
-                        System.out.println("upgrade success");
-                        socket.write("GET https://www.baidu.com/ HTTP/1.1\n" +
-                                "Host: www.baidu.com\n" +
-                                "Connection: keep-alive\n" +
-                                "Cache-Control: max-age=0\n" +
-                                "sec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"\n" +
-                                "sec-ch-ua-mobile: ?0\n" +
-                                "sec-ch-ua-platform: \"macOS\"\n" +
-                                "Upgrade-Insecure-Requests: 1\n" +
-                                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36\n" +
-                                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n" +
-                                "Sec-Fetch-Site: none\n" +
-                                "Sec-Fetch-Mode: navigate\n" +
-                                "Sec-Fetch-User: ?1\n" +
-                                "Sec-Fetch-Dest: document\n" +
-                                "Accept-Encoding: gzip, deflate, br\n" +
-                                "Accept-Language: zh-CN,zh;q=0.9\n" +
-                                "Cookie: BIDUPSID=BCA4E8DD580A6859881E8D4CDA877FBA; PSTM=1625898686; __yjs_duid=1_83d57eb4f6efa627bf47d13cf0d546b71625898824423; BAIDUID=C37569114B7BBE9BA885A20AFB64693D:FG=1; BAIDUID_BFESS=C37569114B7BBE9BA885A20AFB64693D:FG=1; Hm_lvt_aec699bb6442ba076c8981c6dc490771=1629423306; COOKIE_SESSION=10719080_0_9_9_5_27_0_0_9_8_1_8_10719235_0_31_0_1640142177_0_1640142146%7C9%230_0_1640142146%7C1; BD_HOME=1; H_PS_PSSID=35835_34429_35106_35865_34584_35845_35246_35949_35804_35984_35322_26350_35723_35940; BD_UPN=123253; BA_HECTOR=048gak87ag010420u41h20peb0q\n\n");
-                    } else {
-                        rt.cause().printStackTrace();
-                    }
-                });
-            }
+        options.setSsl(true);
+        HttpClient client = vertx.createHttpClient(options);
+        client.request(HttpMethod.GET, 443, "www.bing.com", "/s?ie=utf-8&f=8&rsv_bp=1", r -> {
+            HttpClientRequest request = r.result();
+            request.send(s -> {
+                if (s.succeeded()) {
+                    HttpClientResponse response = s.result();
+                    response.bodyHandler(buf -> {
+                        System.out.println(buf.toString());
+                    });
+
+                }
+            });
+
         });
     }
 }
--- tools-fiddler/src/main/java/com/tuna/tools/fiddler/FiddlerController.java
+++ tools-fiddler/src/main/java/com/tuna/tools/fiddler/FiddlerController.java
@@ -1,13 +1,10 @@
 package com.tuna.tools.fiddler;
 
-import com.tuna.commons.utils.JacksonUtils;
-import io.netty.handler.codec.http.HttpRequest;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.*;
-import io.vertx.core.http.impl.CookieImpl;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.JksOptions;
 import io.vertx.core.net.NetClient;
@@ -31,6 +28,7 @@ public class FiddlerController implements Initializable {
     private TextField bindPortTextField;
     private Vertx vertx;
     private HttpClient httpClient;
+    private Future<NetSocket> proxySocketFuture;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -39,7 +37,6 @@ public class FiddlerController implements Initializable {
     }
 
     public void onWebServerStartClick(MouseEvent event) {
-
         bindPortTextField.getScene().getWindow().setOnCloseRequest(close -> {
             vertx.close(r -> {
                 logger.info("vertx close finish");
@@ -56,13 +53,13 @@ public class FiddlerController implements Initializable {
 
         httpServer.requestHandler(serverRequest -> {
             if (serverRequest.method() == HttpMethod.CONNECT) {
-                logger.info("connect headers: {}", serverRequest.headers().toString());
-                String[] hostPort = serverRequest.headers().get("Host").split(":");
 
-                Future<NetSocket> proxySocketFuture = startSSLNetClient(hostPort[0], hostPort.length > 1 ? Integer.parseInt(hostPort[1]) : 443);
+                if(!serverRequest.host().contains("bing")) {
+                    return;
+                }
                 Future<NetSocket> httpNetSocketFuture = serverRequest.toNetSocket().compose(socket -> socket.upgradeToSsl().map(socket));
 
-                CompositeFuture.all(proxySocketFuture, httpNetSocketFuture).onComplete(cp -> {
+                httpNetSocketFuture.onComplete(cp -> {
                     if (cp.succeeded()) {
                         logger.info("proxy success");
                         proxySocketFuture.result().handler(httpNetSocketFuture.result()::write);
@@ -73,12 +70,18 @@ public class FiddlerController implements Initializable {
                 });
             } else {
                 logger.info("url: {}, method: {}, headers: {}", serverRequest.absoluteURI(), serverRequest.method().name(), serverRequest.headers().toString());
-//                messageHandler(serverRequest);
+                messageHandler(serverRequest);
             }
         });
 
+        HttpClientOptions httpClientOptions = new HttpClientOptions();
+        httpClientOptions.setTrustAll(true);
+        httpClientOptions.setSsl(true);
+        httpClient = vertx.createHttpClient(httpClientOptions);
+
         httpServer.listen(res -> {
             if (res.succeeded()) {
+                proxySocketFuture = startNetClient();
                 logger.info("http server bind at {} success", res.result().actualPort());
             } else {
                 logger.error("http server bind failed", res.cause());
@@ -109,34 +112,10 @@ public class FiddlerController implements Initializable {
         return promise.future();
     }
 
-    private Future<NetSocket> startSSLNetClient(String host, int port) {
-        Promise<NetSocket> promise = Promise.promise();
-
-        NetClientOptions clientOptions = new NetClientOptions();
-        clientOptions.setTrustAll(true);
-        NetClient client = vertx.createNetClient(clientOptions);
-        client.connect(port, host, connectResult -> {
-            if (connectResult.succeeded()) {
-                logger.info("proxy client connect to {}:{} success", host, port);
-                connectResult.result().upgradeToSsl().onComplete( v -> {
-                    if (v.succeeded()) {
-                        promise.complete(connectResult.result());
-                    } else {
-                        promise.fail(v.cause());
-                    }
-                });
-
-            } else {
-                promise.fail(connectResult.cause());
-            }
-        });
-        return promise.future();
-    }
-
     private void messageHandler(HttpServerRequest proxyRequest) {
 //        request.response().end("{\"msg\":1111}");
         RequestOptions options = new RequestOptions().setAbsoluteURI(proxyRequest.absoluteURI().replace("http", "https")).setMethod(proxyRequest.method())
-                .setSsl(true).setPort(443).setHost(proxyRequest.host()).setHeaders(proxyRequest.headers());
+                .setSsl(true).setPort(443).setHost(proxyRequest.host());
         httpClient.request(options, result -> {
             if (result.succeeded()) {
                 HttpClientRequest clientRequest = result.result();
